65a7b1424d4028bddb3041a66289a4e10db479b3
QianW <qian@hhu.edu.cn>
2021-06-02 22:19:32 +0800

    Clean the file used to generate validation set

--- src/generate_observation_ensemble.py
+++ src/generate_observation_ensemble.py
@@ -1,3 +1,4 @@
+# import packages
 """
 RUN SOURCE to generate observation_ensemble.csv
 """
@@ -6,12 +7,9 @@ from veneer.pest_runtime import *
 from veneer.manage import start,kill_all_now
 import os
 
-import source_runner as sr 
-from source_runner import parameter_funcs
-
-from modeling_funcs import vs_settings, \
+from funcs.modeling_funcs import vs_settings, \
     change_param_values, generate_observation_ensemble, generate_parameter_ensemble,\
-        modeling_settings, paralell_vs
+        modeling_settings, paralell_vs, obtain_initials
 
 project_name = 'MW_BASE_RC10.rsproj'
 veneer_name = 'vcmd45\\FlowMatters.Source.VeneerCmd.exe'   
@@ -21,34 +19,30 @@ processes, ports = paralell_vs(first_port, num_copies, project_name, veneer_name
 vs_list = vs_settings(ports, things_to_record)
 
 # generate parameter emsenble
-datapath = 'data/'
+datapath = '../data/'
 nsample = 1000
 param_ensemble = 'samples.csv'
 generate_parameter_ensemble(nsample, param_ensemble, datapath, seed=88)
-para_info = parameter_funcs.load_parameter_file(datapath + 'parameters.csv')
 
 # obtain the initial values of parameters 
-param_names, param_vename_dic, param_vename, param_types = sr.group_parameters(para_info)
-initial_values = parameter_funcs.get_initial_param_vals(vs_list[0], param_names, param_vename, param_vename_dic)
-# initial_values = obtain_initials(vs_list[0])
+initial_values = obtain_initials(vs_list[0])
 
 
 # run to generate observation with default parameter values in the model
 print('------------------Generate observation with default parameter values-----------------')
-retrieve_time = [pd.Timestamp('2000-07-01'), pd.Timestamp('2018-06-30')]
+retrieve_time = [pd.Timestamp('2008-07-01'), pd.Timestamp('2018-06-30')]
 
 # run to generate observation ensemble with parameter ensemble
 print('------------------Generate observation ensemble-----------------')
 obs_ensemble_name = 'DIN_126001A'   
-parameters = pd.read_csv('samples.csv', index_col='index')
+parameters = pd.read_csv('samples.csv', index_col='real_name').iloc[0:2]
 
 # generate the observation ensemble
 def run_obs_ensemble(vs, criteria, start_date, end_date, parameters, 
     obs_ensemble_name, retrieve_time, datapath):
     if not os.path.exists(f'{datapath}{obs_ensemble_name}.csv'):
         load = generate_observation_ensemble(vs_list, criteria, 
-            start_date, end_date, parameters, retrieve_time, initial_values, 
-                param_vename_dic, param_vename, para_info, fromList=fromList)
+            start_date, end_date, parameters, retrieve_time)
         load.to_csv(f'{datapath}{obs_ensemble_name}.csv')
     else:
         print(f'{obs_ensemble_name}.csv exists.')
@@ -59,8 +53,8 @@ run_obs_ensemble(vs_list, criteria, start_date, end_date, parameters,
 
 # set parameter to the initial values
 for vs in vs_list:
-    change_param_values(vs, initial_values, initial_values, param_vename_dic, 
-        param_vename, para_info, fromList=fromList, abs_value=True)
+    change_param_values(vs, initial_values, 
+        fromList=fromList)
 
 kill_all_now(processes)
 
