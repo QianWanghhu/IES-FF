953ea449068cef1359e94552e046066c3073b0d1
QianW <qian@hhu.edu.cn>
2021-06-10 11:02:35 +0800

    collect run_source_* into the run_source file.

--- src/run_source.py
+++ src/run_source.py
@@ -11,12 +11,54 @@ import spotpy as sp
 
 from funcs.read_data import file_settings
 
-def run_source(vars, vs):
-
-    from modeling_funcs import change_param_values, modeling_settings
-
+vs_list = []
+def run_source_cv(vars, vs_list=vs_list):
+    """
+    Script used to run_source and return the output file.
+    The function is called by AdaptiveLejaPCE.
+    """
+    from funcs.modeling_funcs import modeling_settings, generate_observation_ensemble
+    import spotpy as sp
     print('Read Parameters')
-    parameters = pd.read_csv('Parameter_PCE.csv', index_col='Index')
+    parameters = pd.read_csv('../data/Parameters-PCE.csv', index_col='Index')
+
+    # loop over the vars and try to use parallel     
+    parameter_df = pd.DataFrame(index=np.arange(vars.shape[1]), columns=parameters.Name_short)
+    for i in range(vars.shape[1]):
+        parameter_df.iloc[i] = vars[:, i]
+
+    # set the time period of the results
+    retrieve_time = [pd.Timestamp('2017-07-01'), pd.Timestamp('2018-06-30')]
+
+    # define the modeling period and the recording variables
+    _, _, criteria, start_date, end_date = modeling_settings()
+    din = generate_observation_ensemble(vs_list, 
+        criteria, start_date, end_date, parameter_df, retrieve_time)
+
+    if din.mean(axis=0)[0] == 0:
+        cv = np.array([1e-5]) # Set the cv at 1e-5 to satisfy the requirements of the adaptive_polynomial_chaos
+    else:
+        breakpoint()
+        cv = np.array(din.std(axis = 0).values / din.mean(axis = 0).values)
+
+    if cv.shape[0] > 1:
+        breakpoint()
+    cv = cv.reshape(cv.shape[0], 1)
+    print(f'Finish {cv.shape[0]} run')
+
+    return cv
+# END run_source_cv()
+        
+    
+def run_source_lsq(vars, vs_list=vs_list):
+    """
+    Script used to run_source and return the output file.
+    The function is called by AdaptiveLejaPCE.
+    """
+    from funcs.modeling_funcs import modeling_settings, generate_observation_ensemble
+    import spotpy as sp
+    print('Read Parameters')
+    parameters = pd.read_csv('../data/Parameters-PCE.csv', index_col='Index')
 
     # Define objective functions
     # Use annual or monthly loads
@@ -39,47 +81,91 @@ def run_source(vars, vs):
     # End timeseries_sum()
 
     # import observation if the output.txt requires the use of obs.
-    # Here the observation can be monitoring data or synthetic data.
-    date_range = pd.to_datetime(['2009/07/01', '2018/06/30'])
-    observed_din = pd.read_csv('126001A.csv', index_col='Date')
+    date_range = pd.to_datetime(['2017/07/01', '2018/06/30'])
+    observed_din = pd.read_csv(f'{file_settings()[1]}126001A.csv', index_col='Date')
     observed_din.index = pd.to_datetime(observed_din.index)
     observed_din = observed_din.loc[date_range[0]:date_range[1], :].filter(items=[observed_din.columns[0]]).apply(lambda x: 1000 * x)
+    
+    # loop over the vars and try to use parallel     
+    parameter_df = pd.DataFrame(index=np.arange(vars.shape[1]), columns=parameters.Name_short)
+    for i in range(vars.shape[1]):
+        parameter_df.iloc[i] = vars[:, i]
 
-    parameter_dict = {}
-    for i,j in parameters.iterrows():
-        # scaled_value = (j.upper - j.lower) * j.value/100 + j.lower 
-        parameter_dict[j.Name_short] = vars[i]
-        
+    # set the time period of the results
+    retrieve_time = [pd.Timestamp('2017-07-01'), pd.Timestamp('2018-06-30')]
+
     # define the modeling period and the recording variables
-    NODEs, things_to_record, criteria, start_date, end_date = modeling_settings()
-    vs.configure_recording(disable=[{}], enable=things_to_record)
-
-    # subcatchments = [114, 106, 113, 107, 112, 108, 109, 157, 110, 111, 105, 161, 104, 103]
-    vs = change_param_values(vs, parameter_dict) # , subcatchment=subcatchments
-    vs.drop_all_runs()
-    vs.run_model(params={'NoHardCopyResults':True}, start = start_date, end = end_date) 
-
-    # define the criteria for retrieve multiple_time_series
-    column_names = ['din']
-    get_din = vs.retrieve_multiple_time_series(criteria=criteria)
-    get_din.columns = column_names
-    din = get_din.loc[date_range[0]:date_range[1], :]
+    _, _, criteria, start_date, end_date = modeling_settings()
+    din = generate_observation_ensemble(vs_list, 
+        criteria, start_date, end_date, parameter_df, retrieve_time)
 
     # obtain the sum at a given temporal scale
     # din_pbias = sp.objectivefunctions.pbias(observed_din[observed_din.columns[0]], din[column_names[0]])
-    # din_126001A = timeseries_sum(din, temp_scale = 'annual')
+    din_126001A = timeseries_sum(din, temp_scale = 'annual')
+    obs_din = timeseries_sum(observed_din, temp_scale = 'annual')
+    din_126001A = pd.DataFrame(din_126001A,dtype='float')
+    obs_din = pd.DataFrame(obs_din,dtype='float')
 
-    output_file = 'output.txt'
-    output_path = file_settings()[0]
-    if os.path.exists(output_path + output_file):
-        output =  np.loadtxt(f'{output_path}{output_file}')
-        output = np.append(output, din.values, axis=1)
-    
-    np.savetxt(f'{output_path}{output_file}', output)
+    resid = (obs_din - din_126001A).values
+    lsq = np.sum(resid ** 2, axis=0)
+    lsq = lsq.reshape(lsq.shape[0], 1)
 
-    cv = din.std()[0] / din.mean()[0]
+    print(f'Finish {lsq.shape[0]} run')
 
-    return cv
-        
-    
+    return lsq
+# END run_source_lsq()
+
+def run_source_annual(vars, vs_list=vs_list):
+    """
+    Script used to run_source and return the output file.
+    The function is called by AdaptiveLejaPCE.
+    """
+    from funcs.modeling_funcs import modeling_settings, generate_observation_ensemble
+    import spotpy as sp
+    print('Read Parameters')
+    parameters = pd.read_csv('../data/Parameters-PCE.csv', index_col='Index')
+
+    # Define objective functions
+    # Use annual or monthly loads
+    def timeseries_sum(df, temp_scale = 'annual'):
+        """
+        Obtain the sum of timeseries of different temporal scale.
+        temp_scale: str, default is 'Y', monthly using 'M'
+        """
+        assert temp_scale in ['monthly', 'annual'], 'The temporal scale given is not supported.'
+        if temp_scale == 'monthly':
+            sum_126001A = df.resample('M').sum()
+        else:
+            month_126001A = df.resample('M').sum()
+            sum_126001A = pd.DataFrame(index = np.arange(df.index[0].year, df.index[-1].year), 
+                columns=df.columns)
+            for i in range(sum_126001A.shape[0]):
+                sum_126001A.iloc[i, :] = month_126001A.iloc[i*12: (i+1)*12, :].sum()
+
+        return sum_126001A
+    # End timeseries_sum()
+
+    # loop over the vars and try to use parallel     
+    parameter_df = pd.DataFrame(index=np.arange(vars.shape[1]), columns=parameters.Name_short)
+    for i in range(vars.shape[1]):
+        parameter_df.iloc[i] = vars[:, i]
+
+    # set the time period of the results
+    retrieve_time = [pd.Timestamp('2017-07-01'), pd.Timestamp('2018-06-30')]
+
+    # define the modeling period and the recording variables
+    _, _, criteria, start_date, end_date = modeling_settings()
+    din = generate_observation_ensemble(vs_list, 
+        criteria, start_date, end_date, parameter_df, retrieve_time)
+
+    # obtain the sum at a given temporal scale
+    # din_pbias = sp.objectivefunctions.pbias(observed_din[observed_din.columns[0]], din[column_names[0]])
+    din_126001A = timeseries_sum(din, temp_scale = 'annual')
+    din_126001A = pd.DataFrame(din_126001A,dtype='float')
+
+
+    print(f'Finish {din_126001A.shape[0]} run')
+
+    return din_126001A
+# END run_source_annual()
 
