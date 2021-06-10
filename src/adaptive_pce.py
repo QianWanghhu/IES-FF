e7a8013d6a0091334aaf51563d37d42528dab4b5
QianW <qian@hhu.edu.cn>
2021-06-10 11:01:27 +0800

    Build adptive pce for annual loads

--- src/adaptive_pce.py
+++ src/adaptive_pce.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from veneer.pest_runtime import *
+from veneer.manage import start,kill_all_now
 
 import pyapprox as pya
 from functools import partial 
@@ -25,21 +26,36 @@ vs_list = vs_settings(ports, things_to_record)
 # obtain the initial values of parameters 
 initial_values = obtain_initials(vs_list[0])
 
-def run_source(vars, vs_list=vs_list):
+def run_source_annual(vars, vs_list=vs_list):
     """
     Script used to run_source and return the output file.
     The function is called by AdaptiveLejaPCE.
     """
     from funcs.modeling_funcs import modeling_settings, generate_observation_ensemble
+    import spotpy as sp
     print('Read Parameters')
     parameters = pd.read_csv('../data/Parameters-PCE.csv', index_col='Index')
 
-    # import observation if the output.txt requires the use of obs.
-    date_range = pd.to_datetime(['2017/07/01', '2018/06/30'])
-    observed_din = pd.read_csv(f'{file_settings()[1]}126001A.csv', index_col='Date')
-    observed_din.index = pd.to_datetime(observed_din.index)
-    observed_din = observed_din.loc[date_range[0]:date_range[1], :].filter(items=[observed_din.columns[0]]).apply(lambda x: 1000 * x)
-    
+    # Define objective functions
+    # Use annual or monthly loads
+    def timeseries_sum(df, temp_scale = 'annual'):
+        """
+        Obtain the sum of timeseries of different temporal scale.
+        temp_scale: str, default is 'Y', monthly using 'M'
+        """
+        assert temp_scale in ['monthly', 'annual'], 'The temporal scale given is not supported.'
+        if temp_scale == 'monthly':
+            sum_126001A = df.resample('M').sum()
+        else:
+            month_126001A = df.resample('M').sum()
+            sum_126001A = pd.DataFrame(index = np.arange(df.index[0].year, df.index[-1].year), 
+                columns=df.columns)
+            for i in range(sum_126001A.shape[0]):
+                sum_126001A.iloc[i, :] = month_126001A.iloc[i*12: (i+1)*12, :].sum()
+
+        return sum_126001A
+    # End timeseries_sum()
+
     # loop over the vars and try to use parallel     
     parameter_df = pd.DataFrame(index=np.arange(vars.shape[1]), columns=parameters.Name_short)
     for i in range(vars.shape[1]):
@@ -49,23 +65,22 @@ def run_source(vars, vs_list=vs_list):
     retrieve_time = [pd.Timestamp('2017-07-01'), pd.Timestamp('2018-06-30')]
 
     # define the modeling period and the recording variables
-    NODEs, things_to_record, criteria, start_date, end_date = modeling_settings()
+    _, _, criteria, start_date, end_date = modeling_settings()
     din = generate_observation_ensemble(vs_list, 
         criteria, start_date, end_date, parameter_df, retrieve_time)
+
+    # obtain the sum at a given temporal scale
+    # din_pbias = sp.objectivefunctions.pbias(observed_din[observed_din.columns[0]], din[column_names[0]])
+    din_126001A = timeseries_sum(din, temp_scale = 'annual')
+    din_126001A = pd.DataFrame(din_126001A,dtype='float')
+    din_126001A.replace(0, 1e-5, inplace=True)
+
+    print(f'Finish {din_126001A.shape[0]} run')
+    annual_loads = din_126001A.values.T
     breakpoint()
-    if din.mean(axis=0)[0] == 0:
-        cv = np.array([1e-5]) # Set the cv at 1e-5 to satisfy the requirements of the adaptive_polynomial_chaos
-    else:
-        breakpoint()
-        cv = np.array(din.std(axis = 0).values / din.mean(axis = 0).values)
 
-    if cv.shape[0] > 1:
-        breakpoint()
-    cv = cv.reshape(cv.shape[0], 1)
-    print(f'Finish {cv.shape[0]} run')
-
-    return cv
-# END run_source()
+    return annual_loads
+# END run_source_annual()
 
 # read parameter distributions
 datapath = file_settings()[1]
@@ -93,7 +108,7 @@ admissibility_function = partial(
     max_num_samples, err_tol)
 
 refinement_indicator = variance_pce_refinement_indicator
-pce.set_function(run_source, var_trans)
+pce.set_function(run_source_annual, var_trans)
 
 pce.set_refinement_functions(
     refinement_indicator, 
@@ -108,8 +123,8 @@ pce.build()
 import pickle
 pickle.dump(pce, open(f'{file_settings()[0]}\pce-cv.pkl', "wb"))
 
-# pce_load = pickle.load(open(f'{file_settings()[0]}\pce-cv.pkl', "rb"))
-
 # set the parameter values to initial values
 for vs in vs_list:
     vs = change_param_values(vs, initial_values, fromList=True)
+
+kill_all_now(processes)
