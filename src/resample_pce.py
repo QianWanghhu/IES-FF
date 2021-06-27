69e2bbeb19c55bd7713e7b4e98f9eb8c5b3a3ac3
QianW <qian@hhu.edu.cn>
2021-06-27 14:09:44 +0800

    Add the resample.py to update the surrogate with samples of interest

---
+++ src/resample_pce.py
@@ -0,0 +1,62 @@
+
+import numpy as np
+import pandas as pd
+from veneer.pest_runtime import *
+from veneer.manage import start,kill_all_now
+
+import pyapprox as pya
+from pyapprox.utilities import total_degree_space_dimension
+
+from funcs.read_data import file_settings, variables_prep
+from funcs.modeling_funcs import vs_settings, \
+        modeling_settings, paralell_vs, obtain_initials, change_param_values
+
+# Create the copy of models and veneer list
+project_name = 'MW_BASE_RC10.rsproj'
+veneer_name = 'vcmd45\\FlowMatters.Source.VeneerCmd.exe'   
+first_port=15000; num_copies = 2
+_, things_to_record, _, _, _ = modeling_settings()
+processes, ports = paralell_vs(first_port, num_copies, project_name, veneer_name)
+
+vs_list = vs_settings(ports, things_to_record)
+from run_source import run_source_fix_samples, run_source_lsq
+# obtain the initial values of parameters 
+initial_values = obtain_initials(vs_list[0])
+
+# select 10 samples from the optimal sample set
+samples_import = np.loadtxt(f'{file_settings()[0]}adaptive/rmse_iter1/samples_selected.txt')
+rand_index = np.random.randint(0, samples_import.shape[1], 10)
+samples_opt = samples_import[0:13, rand_index]
+vals_opt = run_source_fix_samples(vs_list, samples_opt)
+
+# import PCE and update the surrogate with added training samples
+import pickle
+pce_load = pickle.load(open(f'{file_settings()[0]}\pce-rmse.pkl', "rb"))
+from pyapprox.approximate import approximate
+
+# update training samples
+train_samples = pce_load.variable_transformation.map_from_canonical_space(pce_load.samples)
+train_vals = pce_load.values
+train_samples = np.append(train_samples, samples_opt, axis=1)
+train_vals = np.append(train_vals, vals_opt, axis=0)
+train_set = np.append(train_samples, train_vals.T, axis=0)
+np.savetxt(f'{file_settings()[0]}adaptive/rmse_iter1/train_samples.txt', train_set)
+
+param_file = file_settings()[-1]
+ind_vars, variable = variables_prep(param_file, product_uniform='uniform', dummy=False)
+nfolds = min(10, train_samples.shape[1])
+solver_options = {'cv': nfolds}
+nterms = total_degree_space_dimension(train_samples.shape[0], 2)
+options = {'basis_type': 'expanding_basis', 'variable': ind_vars,
+            'verbosity': 0, 
+            'options': {'max_num_init_terms': nterms,
+            'linear_solver_options': solver_options}}
+
+approx_update = approximate(train_samples, train_vals, method='polynomial_chaos', options=options).approx
+pickle.dump(approx_update, open(f'{file_settings()[0]}adaptive/rmse_iter1/pce-rmse-iter1.pkl', "wb"))
+
+# set the parameter values to initial values
+for vs in vs_list:
+    vs = change_param_values(vs, initial_values, fromList=True)
+
+kill_all_now(processes)
