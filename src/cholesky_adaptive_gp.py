db7bbd50635707d84ff392b0378f1c633d1dc353
QianW <qian@hhu.edu.cn>
2021-07-08 22:30:42 +0800

    Set the code to validate GP with samples from approx_posterior.

--- src/cholesky_adaptive_gp.py
+++ src/cholesky_adaptive_gp.py
@@ -7,6 +7,7 @@ from functools import partial
 import time
 import copy
 import pandas as pd
+import pickle
 
 from scipy.stats import multivariate_normal
 from scipy import stats
@@ -25,11 +26,14 @@ from pyapprox.utilities import \
 from pyapprox.probability_measure_sampling import generate_independent_random_samples_deprecated, rejection_sampling
 from pyapprox.visualization import get_meshgrid_function_data
 from pyapprox import generate_independent_random_samples
+from pyapprox.variables import IndependentMultivariateRandomVariable
 
 import matplotlib as mpl
+from matplotlib import rc
+
 mpl.rcParams['font.size'] = 16
 mpl.rcParams['lines.linewidth'] = 3
-mpl.rcParams['text.usetex'] = True  # use latex for all text handling
+mpl.rcParams['text.usetex'] = False  # use latex for all text handling
 mpl.rcParams['savefig.bbox'] = 'tight'
 mpl.rcParams['savefig.format'] = 'pdf'  # gives best resolution plots
 mpl.rcParams['axes.labelsize'] = 20
@@ -42,87 +46,6 @@ mpl.rcParams['text.latex.preamble'] = \
     r'\usepackage{siunitx}\usepackage{amsmath}\usepackage{amssymb}'
 
 # Qian: to replace the DIN model
-from funcs.read_data import variables_prep, file_settings
-from funcs.modeling_funcs import vs_settings, \
-        modeling_settings, paralell_vs, obtain_initials, change_param_values
-
-# Create the copy of models and veneer list
-project_name = 'MW_BASE_RC10.rsproj'
-veneer_name = 'vcmd45\\FlowMatters.Source.VeneerCmd.exe'   
-first_port=15000; num_copies = 8
-_, things_to_record, _, _, _ = modeling_settings()
-processes, ports = paralell_vs(first_port, num_copies, project_name, veneer_name)
-
-vs_list = vs_settings(ports, things_to_record)
-# obtain the initial values of parameters 
-initial_values = obtain_initials(vs_list[0])
-
-def run_source_lsq(vars, vs_list=vs_list):
-    """
-    Script used to run_source and return the output file.
-    The function is called by AdaptiveLejaPCE.
-    """
-    from funcs.modeling_funcs import modeling_settings, generate_observation_ensemble
-    import spotpy as sp
-    print('Read Parameters')
-    parameters = pd.read_csv('../data/Parameters-PCE.csv', index_col='Index')
-
-    # Define objective functions
-    # Use annual or monthly loads
-    def timeseries_sum(df, temp_scale = 'annual'):
-        """
-        Obtain the sum of timeseries of different temporal scale.
-        temp_scale: str, default is 'Y', monthly using 'M'
-        """
-        assert temp_scale in ['monthly', 'annual'], 'The temporal scale given is not supported.'
-        if temp_scale == 'monthly':
-            sum_126001A = df.resample('M').sum()
-        else:
-            month_126001A = df.resample('M').sum()
-            sum_126001A = pd.DataFrame(index = np.arange(df.index[0].year, df.index[-1].year), 
-                columns=df.columns)
-            for i in range(sum_126001A.shape[0]):
-                sum_126001A.iloc[i, :] = month_126001A.iloc[i*12: (i+1)*12, :].sum()
-
-        return sum_126001A
-    # End timeseries_sum()
-
-    # import observation if the output.txt requires the use of obs.
-    date_range = pd.to_datetime(['2009/07/01', '2018/06/30'])
-    observed_din = pd.read_csv(f'{file_settings()[1]}126001A.csv', index_col='Date')
-    observed_din.index = pd.to_datetime(observed_din.index)
-    observed_din = observed_din.loc[date_range[0]:date_range[1], :].filter(items=[observed_din.columns[0]]).apply(lambda x: 1000 * x)
-    
-    # loop over the vars and try to use parallel     
-    parameter_df = pd.DataFrame(index=np.arange(vars.shape[1]), columns=parameters.Name_short)
-    for i in range(vars.shape[1]):
-        parameter_df.iloc[i] = vars[:, i]
-
-    # set the time period of the results
-    retrieve_time = [pd.Timestamp('2009-07-01'), pd.Timestamp('2018-06-30')]
-
-    # define the modeling period and the recording variables
-    _, _, criteria, start_date, end_date = modeling_settings()
-    din = generate_observation_ensemble(vs_list, 
-        criteria, start_date, end_date, parameter_df, retrieve_time)
-
-    # obtain the sum at a given temporal scale
-    # din_pbias = sp.objectivefunctions.pbias(observed_din[observed_din.columns[0]], din[column_names[0]])
-    din_126001A = timeseries_sum(din, temp_scale = 'annual')
-    obs_din = timeseries_sum(observed_din, temp_scale = 'annual')
-    din_126001A = pd.DataFrame(din_126001A,dtype='float').values
-    obs_din = pd.DataFrame(obs_din,dtype='float').values
-
-    # breakpoint()
-    resid = din_126001A - obs_din
-    rmse = (np.mean(resid ** 2, axis=0)) ** 0.5
-    if rmse[0] == 0: rmse[0] = 1e-8
-    rmse = rmse.reshape(rmse.shape[0], 1)
-
-    print(f'Finish {rmse.shape[0]} run')
-
-    return rmse
-
 def rosenbrock_function(x):
     assert x.shape[0] == 2
     x = 4*x-2
@@ -160,9 +83,9 @@ def convergence_study(kernel, function, sampler,
     # Mesh the input space for evaluations of the real function,
     # the prediction and its MSE
 
-    # Qian:  consider to have external validation samples
-    validation_samples = generate_samples()
-    validation_values = function(validation_samples).squeeze()
+    # Qian: consider to have external validation samples
+    # validation_samples = generate_samples()
+    # validation_values = function(validation_samples).squeeze()
 
     num_samples = np.cumsum(num_new_samples)
     num_steps = num_new_samples.shape[0]
@@ -191,34 +114,38 @@ def convergence_study(kernel, function, sampler,
 
         flag = gp.refine(np.sum(num_new_samples[:sample_step+1]))
         
-        # consider using deepcopy to temporally store a PCE 
-        # flag_curr = copy.deepcopy(flag)
-        # gp_curr = copy.deepcopy(gp)
-
         # allow points to be added to gp more often than gp is evaluated for
         # validation
-        if (sampler.ntraining_samples >= num_samples[sample_step]) & \
-            (sampler.ntraining_samples < num_samples[sample_step + 1]):
-            # flag_curr = copy.deepcopy(flag)
-            # gp_curr = copy.deepcopy(gp)
-
-            if sampler.ntraing_samples
-
-            # Qian: update the weight function and resample
-            pred_values = gp(validation_samples, return_cov=False).squeeze()
-
+        if sampler.ntraining_samples >= num_samples[sample_step]:
+            # validation_values = function(validation_samples).squeeze()
             # Compute error
-            assert pred_values.shape == validation_values.shape
-            error = norm(pred_values-validation_values)/norm(validation_values)
+            # assert pred_values.shape == validation_values.shape
+            # error = norm(pred_values-validation_values)/norm(validation_values)
             if callback is not None:
                 callback(gp)
 
             print(gp.kernel_)
-            print('N', sampler.ntraining_samples, 'Error', error)
-            errors[sample_step] = error
-            nsamples[sample_step] = sampler.ntraining_samples
+             
+            if sample_step >=1:
+                # Compute error
+                gp_load = pickle.load(open(f'gp_{sample_step - 1}.pkl', "rb"))
+                validation_sub = sampler.training_samples[:, num_samples[sample_step - 1]:num_samples[sample_step]]
+                pred_values = gp_load(validation_sub, return_cov=False).squeeze()
+                values_sub = gp(validation_sub, return_cov=False).squeeze()
+                error_gp_comp = norm(pred_values-values_sub)/norm(values_sub)
+                print('-----------error_gp_comp---------', error_gp_comp)
 
-            sample_step += 1
+                print('N', sampler.ntraining_samples, 'Error', error_gp_comp)
+
+            if sample_step >= 1:
+                errors[sample_step -1] = error_gp_comp
+            # else:
+            #     errors[sample_step] = error
+            # nsamples[sample_step] = sampler.ntraining_samples
+                nsamples[sample_step - 1] = num_samples[sample_step -1]
+
+            pickle.dump(gp, open(f'gp_{sample_step}.pkl', "wb"))
+            sample_step += 1            
 
         if flag > 0:
             errors, nsamples = errors[:sample_step], nsamples[:sample_step]
@@ -226,7 +153,7 @@ def convergence_study(kernel, function, sampler,
             break
 
     if return_samples:
-        return errors, nsamples, sampler.training_samples
+        return errors, nsamples, sampler.training_samples[:, 0:num_samples[sample_step - 1]]
 
     return errors, nsamples
 
@@ -337,19 +264,19 @@ def get_posterior_samples(num_vars, weight_function, nsamples):
     return rosenbrock_samples
 
 
-def get_prior_samples(num_vars, variable, nsamples):
-    rosenbrock_samples = generate_independent_random_samples(variable, nsamples)
+def get_prior_samples(num_vars, variables, nsamples):
+    rosenbrock_samples = generate_independent_random_samples(variables, nsamples)
 
     return rosenbrock_samples
 
 def bayesian_inference_example():
     init_scale = 0.1 # used to define length_scale for the kernel
-    num_vars = 13
-    num_candidate_samples = 40000
+    num_vars = 2
+    num_candidate_samples = 10000
     # need to talk with John about how to determine the values for num_new_samples
     num_new_samples = np.asarray([20]+[5]*6+[25]*6+[50]*8)
 
-    nvalidation_samples = 100
+    nvalidation_samples = 10000
 
     prior_pdf = partial(
         tensor_product_pdf, univariate_pdfs=partial(stats.beta.pdf, a=1, b=1))
@@ -363,10 +290,15 @@ def bayesian_inference_example():
         return vals
 
     # Must set variables if not using uniform prior on [0,1]^D
-    variables = None
+    # variables = None
+
+    from scipy.stats import uniform, beta
+    uni_variable = [uniform(0, 1), uniform(0, 1)]
+    variables = IndependentMultivariateRandomVariable(uni_variable)
 
     # Get validation samples from prior
     rosenbrock_samples = get_prior_samples(num_vars, variables, nvalidation_samples + num_candidate_samples)
+    # rosenbrock_samples = get_posterior_samples(num_vars, weight_function, nvalidation_samples + num_candidate_samples)
 
     def generate_random_samples(nsamples, idx=0):
         assert idx+nsamples <= rosenbrock_samples.shape[1]
@@ -403,8 +335,8 @@ def bayesian_inference_example():
     adaptive_cholesky_sampler.set_kernel(copy.deepcopy(kernel))
 
     samplers = [adaptive_cholesky_sampler]
-    methods = ['Prior-Weighted-Cholesky-b']
-    labels = [r'$\mathrm{Prior\;Weighted\;Cholesky}$']
+    methods = ['Learning-Weighted-Cholesky-b']
+    labels = [r'$\mathrm{Adapted\;Weighted\;Cholesky}$']
     fixed_scales = [False]
 
     for sampler, method, fixed_scale in zip(samplers, methods, fixed_scales):
@@ -444,6 +376,44 @@ def bayesian_inference_example():
                  divergences=np.asarray(divergences),
                  cond_nums=np.asarray(cond_nums), samples=samples,
                  temper_params=np.asarray(temper_params))
+
+    fig, axs = plt.subplots(1, 3, figsize=(3*8, 6), sharey=False)
+    styles = ['-']
+    # styles = ['k-','r-.','b--','g:']
+    for method, label, ls, fixed_scale in zip(
+            methods, labels, styles, fixed_scales):
+        filename = get_filename(method, fixed_scale)
+        data = np.load(filename)
+        nsamples, errors = data['nsamples'][:-1], data['errors'][:-1]
+        divergences, cond_nums = data['divergences'][:-1], data['cond_nums'][:-1]
+        # breakpoint()
+        axs[0].loglog(nsamples, errors, ls=ls, label=label)
+        axs[1].loglog(nsamples, divergences, ls=ls, label=label)
+        axs[2].loglog(nsamples, cond_nums, ls=ls, label=label)
+
+    for ii in range(3):
+        axs[ii].set_xlabel(r'$m$')
+        axs[ii].set_xlim(10, 1000)
+    axs[0].set_ylabel(r'$\tilde{\epsilon}_{\omega,2}$', rotation=90)
+    ylim0 = axs[0].get_ylim()
+    ylim1 = axs[1].get_ylim()
+    ylim = [min(ylim0[0], ylim1[0]), max(ylim0[1], ylim1[1])]
+    axs[0].set_ylim(ylim)
+    axs[1].set_ylim(ylim)
+    axs[1].set_ylabel(r'$D_\mathrm{H}$', rotation=90)
+    axs[2].set_ylabel(r'$\kappa$', rotation=90)
+
+    figname = 'bayes_example_comparison_%d.pdf' % num_vars
+    axs[0].legend()
+    plt.savefig(figname)
+
+    method, fixed_scale = 'Learning-Weighted-Cholesky-b', False
+    filename = get_filename(method, fixed_scale)
+    print(filename)
+    adaptive_cholesky_samples = np.load(filename)['samples']
+    temper_params = np.load(filename)['temper_params']
+    nsamples = np.load(filename)['nsamples']
+ 
 
 if __name__ == '__main__':
     try:
