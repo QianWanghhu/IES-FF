b12809807a290abf8d74db45672936352091c92a
QianW <qian@hhu.edu.cn>
2021-07-19 15:15:08 +0800

    Fix the bug in calculating NSE

--- src/cholesky_adaptive_gp.py
+++ src/cholesky_adaptive_gp.py
@@ -9,7 +9,6 @@ import copy
 import pandas as pd
 import pickle
 
-from scipy.stats import multivariate_normal
 from scipy import stats
 # from scipy.optimize import root
 from scipy.optimize import bisect
@@ -26,9 +25,16 @@ from pyapprox.probability_measure_sampling import generate_independent_random_sa
 from pyapprox.visualization import get_meshgrid_function_data
 from pyapprox import generate_independent_random_samples
 from pyapprox.variables import IndependentMultivariateRandomVariable
+from pyapprox.variable_transformations import AffineRandomVariableTransformation
 
 import matplotlib as mpl
 from matplotlib import rc
+import spotpy as sp
+
+from funcs.read_data import variables_prep, file_settings
+from funcs.modeling_funcs import vs_settings, \
+        modeling_settings, paralell_vs, obtain_initials, change_param_values
+
 
 mpl.rcParams['font.size'] = 16
 mpl.rcParams['lines.linewidth'] = 3
@@ -44,13 +50,97 @@ mpl.rcParams['legend.fontsize'] = 16
 mpl.rcParams['text.latex.preamble'] = \
     r'\usepackage{siunitx}\usepackage{amsmath}\usepackage{amssymb}'
 
-# Qian: to replace the DIN model
-def rosenbrock_function(x):
-    assert x.shape[0] == 2
-    x = 4*x-2
-    vals = ((1.-x[0, :])**2+100*(x[1, :]-x[0, :]**2)**2)[:, np.newaxis]
-    # vals = ((1.-x[0,:])**2+1*(x[1,:]-x[0,:]**2)**2)[:,np.newaxis]
-    return vals
+# Create the copy of models and veneer list
+project_name = 'MW_BASE_RC10.rsproj'
+veneer_name = 'vcmd45\\FlowMatters.Source.VeneerCmd.exe'   
+first_port=15000; num_copies = 1
+_, things_to_record, _, _, _ = modeling_settings()
+processes, ports = paralell_vs(first_port, num_copies, project_name, veneer_name)
+
+vs_list = vs_settings(ports, things_to_record)
+# obtain the initial values of parameters 
+initial_values = obtain_initials(vs_list[0])
+
+def run_source_lsq(vars, vs_list=vs_list):
+    """
+    Script used to run_source and return the output file.
+    The function is called by AdaptiveLejaPCE.
+    """
+    from funcs.modeling_funcs import modeling_settings, generate_observation_ensemble
+    import spotpy as sp
+    print('Read Parameters')
+    parameters = pd.read_csv('../data/Parameters-PCE.csv', index_col='Index')
+
+    # Define objective functions
+    # Use annual or monthly loads
+    def timeseries_sum(df, temp_scale = 'annual'):
+        """
+        Obtain the sum of timeseries of different temporal scale.
+        temp_scale: str, default is 'Y', monthly using 'M'
+        """
+        assert temp_scale in ['monthly', 'annual'], 'The temporal scale given is not supported.'
+        if temp_scale == 'monthly':
+            sum_126001A = df.resample('M').sum()
+        else:
+            month_126001A = df.resample('M').sum()
+            sum_126001A = pd.DataFrame(index = np.arange(df.index[0].year, df.index[-1].year), 
+                columns=df.columns)
+            for i in range(sum_126001A.shape[0]):
+                sum_126001A.iloc[i, :] = month_126001A.iloc[i*12: (i+1)*12, :].sum()
+
+        return sum_126001A
+    # End timeseries_sum()
+
+    # Define functions for the objective functions
+    def cal_obj(x_obs, x_mod, obj_type = 'nse'):
+        obj_map = {'nse': sp.objectivefunctions.nashsutcliffe,
+                    'rmse': sp.objectivefunctions.rmse,
+                    'pbias': sp.objectivefunctions.pbias
+                }
+        obj = []
+        for  k in range(x_mod.shape[1]):
+            obj.append(obj_map[obj_type](x_obs, x_mod[:, k].reshape(x_mod.shape[0], 1)))
+        if obj[0] == 0: obj[0] = 1e-8
+        obj = np.array(obj)
+        if obj_type =='nse':
+            obj = 1 - obj
+        obj = obj.reshape(obj.shape[0], 1)
+        print(obj)
+        return obj
+
+    # import observation if the output.txt requires the use of obs.
+    date_range = pd.to_datetime(['2009/07/01', '2018/06/30'])
+    observed_din = pd.read_csv(f'{file_settings()[1]}126001A.csv', index_col='Date')
+    observed_din.index = pd.to_datetime(observed_din.index)
+    observed_din = observed_din.loc[date_range[0]:date_range[1], :].filter(items=[observed_din.columns[0]]).apply(lambda x: 1000 * x)
+    
+    # loop over the vars and try to use parallel     
+    parameter_df = pd.DataFrame(index=np.arange(vars.shape[1]), columns=parameters.Name_short)
+    for i in range(vars.shape[1]):
+        parameter_df.iloc[i] = vars[:, i]
+
+    # set the time period of the results
+    retrieve_time = [pd.Timestamp('2009-07-01'), pd.Timestamp('2018-06-30')]
+
+    # define the modeling period and the recording variables
+    _, _, criteria, start_date, end_date = modeling_settings()
+    din = generate_observation_ensemble(vs_list, 
+        criteria, start_date, end_date, parameter_df, retrieve_time)
+
+    # obtain the sum at a given temporal scale
+    # din_pbias = sp.objectivefunctions.pbias(observed_din[observed_din.columns[0]], din[column_names[0]])
+    din_126001A = timeseries_sum(din, temp_scale = 'annual')
+    obs_din = timeseries_sum(observed_din, temp_scale = 'annual')
+    din_126001A = pd.DataFrame(din_126001A,dtype='float').values
+    obs_din = pd.DataFrame(obs_din,dtype='float').values
+
+    # breakpoint()
+    obj = cal_obj(obs_din, din_126001A, obj_type = 'nse')
+    breakpoint()
+    print(f'Finish {obj.shape[0]} run')
+
+    return obj
+# END run_source_lsq()
 
 def convergence_study(kernel, function, sampler,
                       num_vars, generate_samples, num_new_samples,
@@ -232,22 +322,34 @@ def get_prior_samples(num_vars, variables, nsamples):
     return rosenbrock_samples
 
 def bayesian_inference_example():
+    # read parameter distributions
+    datapath = file_settings()[1]
+    para_info = pd.read_csv(datapath + 'Parameters-PCE.csv')
+
+    # define the variables for PCE
+    param_file = file_settings()[-1]
+    ind_vars, variables = variables_prep(param_file, product_uniform='uniform', dummy=False)
+    var_trans = AffineRandomVariableTransformation(variables, enforce_bounds=True)
     init_scale = 0.1 # used to define length_scale for the kernel
-    num_vars = 2
-    num_candidate_samples = 10000
-    num_new_samples = np.asarray([20]+[5]*6+[25]*6+[50]*8)
+    num_vars = variables.nvars
+    num_candidate_samples = 20000
+    num_new_samples = np.asarray([2]+[5]*6+[15]*6+[25]*8)
 
     nvalidation_samples = 10000
 
-    prior_pdf = partial(
-        tensor_product_pdf, univariate_pdfs=partial(stats.beta.pdf, a=1, b=1))
+    from scipy import stats 
+    # breakpoint()
+    prior_pdf = partial(tensor_product_pdf, 
+        univariate_pdfs=[partial(stats.uniform.pdf, *ind_vars[ii].args) for ii in range(num_vars)])
 
     # Must set variables if not using uniform prior on [0,1]^D
     # variables = None
 
-    from scipy.stats import uniform, beta, norm
-    uni_variable = [uniform(0, 1), uniform(0, 1)]
-    variables = IndependentMultivariateRandomVariable(uni_variable)
+    # prior_pdf = partial(
+    #     tensor_product_pdf, univariate_pdfs=partial(stats.beta.pdf, a=1, b=1))
+    # from scipy.stats import uniform, beta, norm
+    # uni_variable = [uniform(0, 1), uniform(0, 1)]
+    # variables = IndependentMultivariateRandomVariable(uni_variable)
 
     # Get validation samples from prior
     rosenbrock_samples = get_prior_samples(num_vars, variables, nvalidation_samples + num_candidate_samples)
@@ -304,7 +406,7 @@ def bayesian_inference_example():
                 print(temper_params)
 
         errors, nsamples, samples = convergence_study(
-            kernel, rosenbrock_function, sampler, num_vars,
+            kernel, run_source_lsq, sampler, num_vars,
             generate_validation_samples, num_new_samples,
             update_kernel_scale_num_samples, callback=callback,
             return_samples=True)
