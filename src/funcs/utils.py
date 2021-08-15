473bcae498a3b8829af6650537fc4395d062fbfa
QianW <qian@hhu.edu.cn>
2021-08-15 20:13:51 +0800

    Add partial_rank to utils

--- src/funcs/utils.py
+++ src/funcs/utils.py
@@ -6,6 +6,8 @@ import pyapprox as pya
 import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from toposort import toposort
 
 from .read_data import file_settings
 
@@ -57,7 +59,7 @@ def dotty_plot(x_samples, y_vals, x_opt, y_opt, param_names, y_lab='SSE', orig_x
             ax = sns.scatterplot(x=orig_x_opt[ii, :], y=orig_y_opt.flatten(), ax=axes[ii // 4, ii % 4], color='g', s=20, alpha=0.3)
             
         ax = sns.scatterplot(x=x_samples[ii, :], y=y_vals.flatten(), ax=axes[ii // 4, ii % 4], s=20, alpha=0.7)
-        ax = sns.scatterplot(x=x_opt[ii, :], y=y_opt.flatten(), ax=axes[ii // 4, ii % 4], color='r', s=20)
+        # ax = sns.scatterplot(x=x_opt[ii, :], y=y_opt.flatten(), ax=axes[ii // 4, ii % 4], color='r', s=20)
                    
         ax.set_title(param_names[ii])
         if ii % 4 == 0: ax.set_ylabel(y_lab)
@@ -179,3 +181,56 @@ def update_bounds(f_name, bounds):
             pickle.dump(bounds_dict, f)
     return bounds_dict
 # End update_bounds
+
+# Define helper function for partial sorting
+def partial_rank(sa, len_params, conf_level=0.95):
+    """
+    Help function for partial ranking.
+    Parameters:
+    ===========
+    sa: ndarray, matrix of sensitivity indices, 
+        of shape (N * P) where N is the number of bootstraps, P is the number of parameters
+    len_params: int, the number of parameters
+    conf_level: float, the confidence level used for the calculation of confidence intervals of rankings
+
+    Returns:
+    ========
+    partial_rankings: dict, dictionary of partial rankings of parameters
+                    e.g. {0:['2'], 1:['0', '1']} 
+                        which means the parameter of index 2 is in group 0 which is more sensitive than the group 1 
+    """
+    num_boot = sa.shape[0]
+    # print(num_boot)
+    rankings = np.zeros([num_boot, len_params])
+    ranking_ci = np.zeros([2, len_params])
+    for resample in range(num_boot):
+        rankings[resample,:] = np.argsort(sa[resample,:]).argsort()
+    
+    ## Generate confidence intervals of rankings based on quantiles (if the num_resample is big enough)
+    ranking_ci = np.quantile(rankings,[(1-conf_level)/2, 0.5 + conf_level/2], axis=0)
+    # rci = norm.ppf(0.5 + conf_level / 2) * rankings.std(ddof=1, axis=0)
+    # ranking_ci = [rankings.mean(axis=0) - rci, rankings.mean(axis=0) + rci]
+    # ranking_ci = np.rint(ranking_ci)
+    # ranking_ci[ranking_ci<0] = 0        
+    conf_low = ranking_ci[0]
+    conf_up = ranking_ci[1]
+    rank_conf = {j:None for j in range(len_params)}
+    abs_sort= {j:None for j in range(len_params)}
+
+    for j in range(len_params):
+        rank_conf[j] = [conf_low[j], conf_up[j]]  
+    # End for
+
+    for m in range(len_params):
+        list_temp = np.where(conf_low >= conf_up[m])
+        set_temp = set()
+        if len(list_temp) > 0:
+            for ele in list_temp[0]:
+                set_temp.add(ele)
+        abs_sort[m] = set_temp
+        # End if
+    # End for
+
+    order_temp = list(toposort(abs_sort))
+    partial_rankings = {j: list(order_temp[j]) for j in range(len(order_temp))}
+    return partial_rankings
