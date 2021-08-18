635f8ab7ab898cd98b1643240e5b54331fcaa636
QianW <qian@hhu.edu.cn>
2021-08-18 16:10:38 +0800

    Tried the use of analytic based sobol' indices with GP

--- src/plot_gp.py
+++ src/plot_gp.py
@@ -30,17 +30,41 @@ mpl.rcParams['text.latex.preamble'] = \
     r'\usepackage{siunitx}\usepackage{amsmath}\usepackage{amssymb}'
 
 from funcs.read_data import file_settings, variables_prep
-from funcs.utils import partial_rank
+from funcs.utils import partial_rank, return_sa
 from gp_pce_model import *
+
 fpath = '../output/gp_run_0813/'
 gp = pickle.load(open(f'{fpath}gp_1.pkl', "rb"))
 x_training = gp.X_train_
 y_training = gp.y_train_
 
 
-gp1 = pickle.load(open(f'{fpath}gp_0.pkl', "rb"))
+gp1 = pickle.load(open(f'../output/gp_run_0810/gp_0.pkl', "rb"))
+# gp1 = pickle.load(open(f'{fpath}gp_0.pkl', "rb"))
 x1_training = gp1.X_train_
 y1_training = gp1.y_train_
+# x_training[:, 0:2] = x_training[:, 0:2] * 100
+x_select = x1_training[np.where(y1_training > -1000)[0], :]
+y_eval = y1_training[y1_training>-1000]
+y_hat = gp.predict(x_select)
+plt.plot(y_eval.flatten(), y_hat.flatten(), linestyle='', marker='o', ms=8)
+
+# Scatter plot of validation
+x_hat = x1_training[-40:, :]
+# x_hat = x1_training[np.where(y1_training>-1)[0], :]
+y_eval = y1_training[-40:]
+y_hat, y_cov = gp.predict(x_hat, return_std=True)
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+ax.plot(y_eval.flatten(), y_hat.flatten(), linestyle='', marker='o', ms=8)
+ax.set_xlabel('Model outputs')
+ax.set_ylabel('GPR simulation')
+ax.plot(np.linspace(-3, 1, 100), np.linspace(-3, 1, 100), linestyle='--', color='orange', alpha=0.7)
+
+plt.savefig(f'{fpath}figs/validation_full_range', dpi=300)
+
+# save validation samples
+vali_samples = np.append(x_hat, y1_training[-200:], axis=1)
+np.savetxt('validation_samples.txt', vali_samples)
 
 # histplot of y training values
 fig, axes = plt.subplots(1, 2, figsize=(16, 6))
@@ -52,7 +76,7 @@ axes[0].set_title('(a)')
 axes[1].hist(y_training[y_training>-10].flatten())
 axes[1].set_xlabel('Viney F')
 axes[1].set_title('(b)') 
-plt.savefig(f'{fpath}figs/training_vals', dpi=300)
+plt.savefig(f'{fpath}figs/training_vals_full_range', dpi=300)
 
 # Resample in the ranges where the objective values are above 0
 x_select = x_training[np.where(y_training>0)[0], :]
@@ -65,26 +89,20 @@ variable_temp = pyapprox.IndependentMultivariateRandomVariable(univariable_temp)
 param_file = file_settings()[-1]
 ind_vars, variables = variables_prep(param_file, product_uniform='uniform', dummy=False)
 var_trans = AffineRandomVariableTransformation(variables, enforce_bounds=True)
-filename = f'{file_settings()[0]}sa_gp.npz'
+filename = f'{file_settings()[0]}sa_gp2.npz'
 if not os.path.exists(filename):
     order = 2
     interaction_terms = pya.compute_hyperbolic_indices(len(ind_vars), order)
     interaction_terms = interaction_terms[:, np.where(
     interaction_terms.max(axis=0) == 1)[0]]
-    sa = pyapprox.sampling_based_sobol_indices_from_gaussian_process(gp, 
-        variable_temp, interaction_terms=interaction_terms, nsamples=1000, 
-            ngp_realizations=10, ninterpolation_samples = 100, nsobol_realizations = 100)
-    np.savez(filename, total_effects=sa['total_effects']['values'])
+    sa = pyapprox.analytic_sobol_indices_from_gaussian_process(gp, variables, interaction_terms, 
+        ngp_realizations=100, ninterpolation_samples=100, use_cholesky=False, ncandidate_samples=10000)
+    # np.savez(filename, total_effects=sa['total_effects']['values'])
 else:
     data = np.load(filename, allow_pickle=True)
     sa = data
 
-ST_values = sa['total_effects']['values']
-# reshape the matrix as N*P
-ST = np.zeros(shape=(ST_values.shape[0] * ST_values.shape[2], ST_values.shape[1]))
-for ii in range(ST_values.shape[2]):
-        ST[ii*100:(ii+1)*100, :] = ST_values[:, :, ii].\
-            reshape(ST_values.shape[0], ST_values.shape[1])
+ST = sa['total_effects']['values']
 index_sort = partial_rank(ST, ST.shape[1], conf_level=0.95)
 
 # define the order to fix parameters
