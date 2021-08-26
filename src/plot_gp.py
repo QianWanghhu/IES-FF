7cd034221d973e83cf034360d2833457a5861b4e
QianW <qian@hhu.edu.cn>
2021-08-26 18:51:14 +0800

    Set dotty_plot only for optimal values.

--- src/plot_gp.py
+++ src/plot_gp.py
@@ -39,48 +39,104 @@ x_training = gp.X_train_
 y_training = gp.y_train_
 
 def plot(gp):
-    gp1 = pickle.load(open(f'../output/gp_run_0813/gp_0.pkl', "rb"))
+    gp1 = pickle.load(open(f'../output/gp_run_0819/gp_0.pkl', "rb"))
     x1_training = gp1.X_train_
     y1_training = gp1.y_train_
-    x_select = x1_training[np.where(y1_training > -100)[0], :]
-    y_eval = y1_training[y1_training>-100]
-    y_hat = gp.predict(x_select)
+    y_hat = gp.predict(x1_training)[50:150]
+    # x_hat = x1_training[np.where(y_hat>0)[0][0:100], :]
+    # y_eval = y1_training[y_hat>0][0:100]
+    # y_hat = y_hat[y_hat>0][0:100]
+    y_eval = y1_training[50:150]
     np.linalg.norm(y_hat.flatten() - y_eval.flatten()) / np.linalg.norm(y_eval.flatten())
-    plt.plot(y_eval.flatten(), y_hat.flatten(), linestyle='', marker='o', ms=8)
-    plt.plot(np.linspace(-100, 1, 100), np.linspace(-100, 1, 100), linestyle='--', color='orange', alpha=0.7)
-
-
-    # Scatter plot of validation
-    x_hat = x1_training[-40:, :]
-    # x_hat = x1_training[np.where(y1_training>-1)[0], :]
-    y_eval = y1_training[-40:]
-    y_hat, y_cov = gp.predict(x_hat, return_std=True)
     fig, ax = plt.subplots(1, 1, figsize=(8, 6))
     ax.plot(y_eval.flatten(), y_hat.flatten(), linestyle='', marker='o', ms=8)
-    ax.set_xlabel('Model outputs')
+    ax.set_xlabel('Modelled Viney_F')
     ax.set_ylabel('GPR simulation')
-    ax.plot(np.linspace(-3, 1, 100), np.linspace(-3, 1, 100), linestyle='--', color='orange', alpha=0.7)
-
-    plt.savefig(f'{fpath}figs/validation_full_range', dpi=300)
+    y_eval_opt = y_eval[y_eval>0.382]
+    y_hat_opt = y_hat[y_eval>0.382]
+    ax.plot(y_eval_opt.flatten(), y_hat_opt.flatten(), linestyle='', 
+        marker='o', color='darkorange', alpha=0.7, ms=8)
+    ax.plot(np.linspace(y_eval.min(), 0.8, 100), np.linspace(y_eval.min(), 0.8, 100), 
+        linestyle='--', color='slategrey', alpha=0.5)
+    plt.savefig(f'{fpath}figs/gpr_validation_full_range', dpi=300)
 
     # save validation samples
-    vali_samples = np.append(x_hat, y1_training[-200:], axis=1)
+    vali_samples = np.append(x_hat, y_eval.reshape(y_eval.shape[0], 1), axis=1)
     np.savetxt('validation_samples.txt', vali_samples)
 
-    # histplot of y training values
-    fig, axes = plt.subplots(1, 2, figsize=(16, 6))
-    y_plot = 1 - y_training
-    axes[0].hist(y_training.flatten())
-    axes[0].set_xlabel('Viney F')
-    axes[0].set_ylabel('Counts')
-    axes[0].set_title('(a)') 
-    axes[1].hist(y_training[y_training>-10].flatten())
-    axes[1].set_xlabel('Viney F')
-    axes[1].set_title('(b)') 
-    plt.savefig(f'{fpath}figs/training_vals_full_range', dpi=300)
+# END plot()     
+
+# define the order to fix parameters
+def fix_plot(gp, variable_temp):
+    from funcs.utils import dotty_plot, define_constants, fix_sample_set
+    dot_fn = f'{file_settings()[0]}gp_run_0816/dotty_samples.txt'
+    if not os.path.exists(dot_fn):
+        dot_samples = generate_independent_random_samples(variable_temp, 100000)
+        np.savetxt(dot_fn, dot_samples)
+    else:
+        dot_samples = np.loadtxt(dot_fn)
+    dot_vals = np.zeros(shape=(dot_samples.shape[1], 1))
+    for ii in range(10):
+        dot_vals[10000*ii:(ii+1)*10000] = gp.predict(dot_samples[:, 10000*ii:(ii+1)*10000].T)
+    # dot_samples = dot_samples[:, np.where(dot_vals > 0)[0]]
+    # dot_vals = dot_vals[dot_vals>0]
+    samples_opt = dot_samples[:, np.where(dot_vals>=0.382)[0]]
+    vals_opt = dot_vals[dot_vals>=0.382]
+    print(f'Number of values beyond the threshold: {samples_opt.shape[0]}')
+    x_default = define_constants(dot_samples, stats = 'median')
+    num_opt = []
+    vals_dict = {}
+    index_fix = np.array([], dtype=int)
+    pct_optimal = {}
+
+    for ii in range(max(index_sort.keys()), 0, -1):
+        index_fix = np.append(index_fix, index_sort[ii])
+        print(f'Fix {index_fix.shape[0]} parameters')
+        print(f'index: {index_fix}')
+        samples_fix = fix_sample_set(index_fix, dot_samples, x_default)
+        vals_fix = np.zeros_like(dot_vals)
+        # calculate with surrogate 
+        vals_fix = gp.predict(samples_fix.T)
+        for ii in range(10):
+            vals_fix[10000*ii:(ii+1)*10000] = gp.predict(samples_fix[:, 10000*ii:(ii+1)*10000].T)
+                    
+        # select points statisfying the optima
+        index_opt_fix = np.where(vals_fix.flatten() >= 0.382)[0]
+        samples_opt_fix = samples_fix[:, index_opt_fix]
+        vals_opt_fix = vals_fix[index_opt_fix]
+        vals_dict[f'fix_{len(index_fix)}'] = vals_opt_fix
+                                                                                                                                                                                                                                                                                                                                                    
+        # plot     
+        fig = dotty_plot(samples_opt, vals_opt.flatten(), 
+            samples_opt_fix,vals_opt_fix.flatten(), param_names, 'Viney_F');
+        plt.savefig(f'{fpath}/fix_median_full/{len(index_fix)}.png', dpi=300)
+        # calculate the ratio of optimal values
+        pct_optimal[f'fix_{len(index_fix)}'] = vals_opt_fix.shape[0] / dot_vals.shape[0]
+    # END For
+
+    pct_optimal = pd.DataFrame.from_dict(pct_optimal, orient='index', columns=['Proportion'])
+    pct_optimal.to_csv(f'{file_settings()[0]}gp_run_0816/fix_median_full/Proportion_optimal.csv')
+    # PDF plot
+    fig, axes = plt.subplots(1, 3, figsize=(20, 6), sharex=True)
+    sns.distplot(dot_vals.flatten(), hist=False,  ax=axes[0])
+    k = 0
+    for key, value in vals_dict.items():
+        sns.distplot(value.flatten(), hist=False, ax=axes[k//4]);
+        k += 1
+
+    axes[0].legend(['Uncond', *list(vals_dict.keys())[0:4]])
+    axes[1].set_xlabel('Viney_F')
+    axes[1].set_ylabel('')
+    axes[1].legend(list(vals_dict.keys())[4:8])
+    axes[2].legend(list(vals_dict.keys())[8:])
+    axes[2].set_ylabel('')
+    # for ii in range(3):
+    #     axes[ii].plot(0.382, np.linspace(-1, 1))
+    plt.savefig(f'{fpath}/fix_median_full/objective_dist.png', dpi=300)
+
 
 # Resample in the ranges where the objective values are above 0
-x_select = x_training[np.where(y_training>-1)[0], :]
+x_select = x_training[np.where(y_training>0)[0], :]
 x_range = x_select.max(axis=0)
 univariable_temp = [stats.uniform(0, x_range[ii]) for ii in range(0, x_range.shape[0])]
 variable_temp = pyapprox.IndependentMultivariateRandomVariable(univariable_temp)
@@ -115,56 +171,5 @@ else:
     index_sort = {}
     for k, v in index_sort_load.items():
         index_sort[int(k)] = index_sort_load[k]
-        
-
-# define the order to fix parameters
-def fix_plot(gp, variable_temp):
-    from funcs.utils import dotty_plot, define_constants, fix_sample_set
-    dot_samples = generate_independent_random_samples(variable_temp, 100000)
-    dot_vals = gp.predict(dot_samples.T)
-    # dot_samples = dot_samples[:, np.where(dot_vals > 0)[0]]
-    # dot_vals = dot_vals[dot_vals>0]
-    samples_opt = dot_samples[:, np.where(dot_vals>0)[0]]
-    x_default = define_constants(samples_opt, stats = 'median')
-    num_opt = []
-    vals_dict = {}
-    index_fix = np.array([], dtype=int)
-
-    for ii in range(max(index_sort.keys()), 0, -1):
-        index_fix = np.append(index_fix, index_sort[ii])
-        print(f'Fix {index_fix.shape[0]} parameters')
-        print(f'index: {index_fix}')
-        samples_fix = fix_sample_set(index_fix, dot_samples, x_default)
-        vals_fix = np.zeros_like(dot_vals)
-        # calculate with PCE 
-        vals_fix = gp.predict(samples_fix.T)
-            
-        vals_dict[f'fix_{len(index_fix)}'] = vals_fix
-        # select points statisfying the optima
-        index_opt_fix = np.where(vals_fix.flatten() >= 0.382)
-        num_opt.append(index_opt_fix[0].shape[0])
-        samples_opt_fix = samples_fix[:, index_opt_fix[0]]
-        vals_opt_fix = vals_fix[index_opt_fix]
-                                                                                                                                                                                                                                                                                                                                                    
-        # plot     
-        fig = dotty_plot(samples_fix, np.log((1-vals_fix).flatten()), 
-            samples_opt_fix, np.log((1-vals_opt_fix).flatten()), param_names, 'ln(1 - Viney_F)', orig_x_opt=dot_samples, orig_y_opt=np.log((1-dot_vals).flatten()));
-        plt.savefig(f'{fpath}/{len(index_fix)}.png', dpi=300)
-
-    # PDF plot
-    fig, axes = plt.subplots(1, 3, figsize=(20, 6), sharex=True)
-    sns.distplot(np.log((1 - dot_vals).flatten()), hist=False,  ax=axes[0])
-    k = 0
-    for key, value in vals_dict.items():
-        sns.distplot(np.log((1 - value).flatten()), hist=False, ax=axes[k//4]);
-        k += 1
-
-    axes[0].legend(['Uncond', *list(vals_dict.keys())[0:4]])
-    axes[1].set_xlabel('ln(1 - Viney_F)')
-    axes[1].set_ylabel('')
-    axes[1].legend(list(vals_dict.keys())[4:8])
-    axes[2].legend(list(vals_dict.keys())[8:])
-    axes[2].set_ylabel('')
-    plt.savefig(f'{fpath}/objective_dist.png', dpi=300)
 
 fix_plot(gp, variable_temp)
