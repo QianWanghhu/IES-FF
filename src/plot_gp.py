97629b3ba7094dca778af86eac8f8a6cea3a8059
QianW <qian@hhu.edu.cn>
2021-09-11 09:30:23 +0800

    Add the function for calculating samples in the subregion.

--- src/plot_gp.py
+++ src/plot_gp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from numba.core.typing.templates import BaseRegistryLoader
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -33,24 +34,24 @@ from funcs.read_data import file_settings, variables_prep
 from funcs.utils import partial_rank, return_sa
 from gp_pce_model import *
 
-fpath = '../output/gp_run_0816/'
-gp = pickle.load(open(f'{fpath}gp_1.pkl', "rb"))
-x_training = gp.X_train_
-y_training = gp.y_train_
 
-def plot(gp):
+def plot(gp, fpath, plot_range='full', save_vali=False):
     gp1 = pickle.load(open(f'../output/gp_run_0819/gp_0.pkl', "rb"))
     x1_training = gp1.X_train_
     y1_training = gp1.y_train_
-    y_hat = gp.predict(x1_training)[50:150]
-    # x_hat = x1_training[np.where(y_hat>0)[0][0:100], :]
-    # y_eval = y1_training[y_hat>0][0:100]
-    # y_hat = y_hat[y_hat>0][0:100]
-    y_eval = y1_training[50:150]
+    if plot_range == 'full':
+        y_hat = gp.predict(x1_training)[50:150]
+        y_eval = y1_training[50:150]
+    else:
+        y_hat = gp.predict(x1_training)
+        x_hat = x1_training[np.where(y_hat>0)[0][0:100], :]
+        y_eval = y1_training[y_hat>0][0:100]
+        y_hat = y_hat[y_hat>0][0:100]
+
     np.linalg.norm(y_hat.flatten() - y_eval.flatten()) / np.linalg.norm(y_eval.flatten())
     fig, ax = plt.subplots(1, 1, figsize=(8, 6))
     ax.plot(y_eval.flatten(), y_hat.flatten(), linestyle='', marker='o', ms=8)
-    ax.set_xlabel('Modelled Viney_F')
+    ax.set_xlabel('Modelled F')
     ax.set_ylabel('GPR simulation')
     y_eval_opt = y_eval[y_eval>0.382]
     y_hat_opt = y_hat[y_eval>0.382]
@@ -61,13 +62,48 @@ def plot(gp):
     plt.savefig(f'{fpath}figs/gpr_validation_full_range', dpi=300)
 
     # save validation samples
-    vali_samples = np.append(x_hat, y_eval.reshape(y_eval.shape[0], 1), axis=1)
-    np.savetxt('validation_samples.txt', vali_samples)
-
+    if save_vali:
+        vali_samples = np.append(x_hat, y_eval.reshape(y_eval.shape[0], 1), axis=1)
+        np.savetxt('validation_samples.txt', vali_samples)
 # END plot()     
 
+def vali_samples_subreg(gp, variable, num_candidate_samples=2000):
+    candidates_samples = generate_gp_candidate_samples(gp, gp.X_train_.shape[1], 
+        num_candidate_samples = num_candidate_samples, 
+            generate_random_samples=None, variables=variable)
+    
+    y_pred = gp.predict(candidates_samples)
+    samples_vali_subreg = candidates_samples[y_pred[y_pred>0][0:100], :]
+    samples_vali_full = candidates_samples[0:100, :]
+    vali_samples = np.zeros(shape=(200, 14))
+    vali_samples[0:100, 0:13] = samples_vali_subreg
+    vali_samples[100:200, 0:13] = samples_vali_full
+    vali_samples[0:100, 13] = y_pred[y_pred>0][0:100]
+    vali_samples[100:200, 13] = y_pred[0:100]
+    return vali_samples
+# END vali_samples_subreg()
+
+# Calculate the ratio of samples in the subregion
+def ratio_subreg(gp):
+    y_training = gp.y_train_
+    num_new_samples = np.asarray([0]+[20]+[8]*10+[16]*20+[24]*16+[40]*14)
+    num_samples = np.cumsum(num_new_samples)
+    ratio_samples = np.zeros(shape=(num_new_samples.shape[0]-2, 2))
+    ratio_sum = 0
+    for ii in range(num_new_samples.shape[0] - 2):
+        num_subreg = np.where(y_training[num_samples[ii]: num_samples[ii+1]]>0)[0].shape[0]
+        ratio_sum = ratio_sum + num_subreg
+        ratio_samples[ii, 0] = num_subreg / num_new_samples[ii+1]
+        ratio_samples[ii, 1] = ratio_sum / num_samples[ii+1]
+
+    ratio_df = pd.DataFrame(data=ratio_samples, 
+        index=np.arange(ratio_samples.shape[0]), columns=['Subregion', 'FullSpcace'])
+    ratio_df['num_samples'] = num_samples[1:-1]
+    return ratio_df
+# END ratio_subreg()
+
 # define the order to fix parameters
-def fix_plot(gp, variable_temp):
+def fix_plot(gp, variable_temp, plot_range='full'):
     from funcs.utils import dotty_plot, define_constants, fix_sample_set
     dot_fn = f'{file_settings()[0]}gp_run_0816/dotty_samples.txt'
     if not os.path.exists(dot_fn):
@@ -82,9 +118,13 @@ def fix_plot(gp, variable_temp):
     # dot_vals = dot_vals[dot_vals>=0.382]
     samples_opt = dot_samples[:, np.where(dot_vals>=0.382)[0]]
     vals_opt = dot_vals[dot_vals>=0.382]
-    breakpoint()
     print(f'Number of values beyond the threshold: {samples_opt.shape[0]}')
-    x_default = define_constants(dot_samples, stats = 'median')
+    if plot_range == 'full':
+        x_default = define_constants(dot_samples, stats = 'median')
+        fig_path = 'fix_median_full'
+    else:
+        x_default = define_constants(samples_opt, stats = 'median')
+        fig_path = 'fix_median_subreg'
     num_opt = []
     vals_dict = {}
     index_fix = np.array([], dtype=int)
@@ -109,32 +149,45 @@ def fix_plot(gp, variable_temp):
                                                                                                                                                                                                                                                                                                                                                     
         # plot     
         fig = dotty_plot(samples_opt, vals_opt.flatten(), samples_opt_fix, vals_opt_fix.flatten(), 
-            param_names, 'Viney_F', orig_x_opt=samples_fix, orig_y_opt=vals_fix);
-        plt.savefig(f'{fpath}/fix_median_full/{len(index_fix)}.png', dpi=300)
+            param_names, 'F', orig_x_opt=samples_fix, orig_y_opt=vals_fix);
+        # plt.savefig(f'{fpath}/{fig_path}/{len(index_fix)}.png', dpi=300)
         # calculate the ratio of optimal values
         pct_optimal[f'fix_{len(index_fix)}'] = vals_opt_fix.shape[0] / dot_vals.shape[0]
-    # END For
+    # # END For
 
     pct_optimal = pd.DataFrame.from_dict(pct_optimal, orient='index', columns=['Proportion'])
-    pct_optimal.to_csv(f'{file_settings()[0]}gp_run_0816/fix_median_full/Proportion_optimal.csv')
+    pct_optimal.to_csv(f'{file_settings()[0]}gp_run_0816/{fig_path}/Proportion_optimal.csv')
     # PDF plot
     fig, axes = plt.subplots(1, 3, figsize=(20, 6), sharex=True)
     sns.distplot(vals_opt.flatten(), hist=False,  ax=axes[0])
     k = 0
+    df_stats = pd.DataFrame(columns=['mean', 'std', 'qlow','qup'])
+    df_stats.loc['uncond', ['mean', 'std']] = [vals_opt.mean(), vals_opt.std()]
+    df_stats.loc['uncond', 'qlow':'qup'] = np.quantile(vals_opt, [0.025, 0.957])
     for key, value in vals_dict.items():
         sns.distplot(value.flatten(), hist=False, ax=axes[k//4]);
+        df_stats.loc[key, 'mean'] = value.mean()
+        df_stats.loc[key, 'std'] = value.std()
+        df_stats.loc[key, 'qlow':'qup'] = np.quantile(value, [0.025, 0.975])
         k += 1
 
     axes[0].legend(['Uncond', *list(vals_dict.keys())[0:4]])
-    axes[1].set_xlabel('Viney_F')
+    axes[1].set_xlabel('F')
     axes[1].set_ylabel('')
     axes[1].legend(list(vals_dict.keys())[4:8])
     axes[2].legend(list(vals_dict.keys())[8:])
     axes[2].set_ylabel('')
-    # for ii in range(3):
-    #     axes[ii].plot(0.382, np.linspace(-1, 1))
-    plt.savefig(f'{fpath}/fix_median_full/objective_dist.png', dpi=300)
+    for ii in range(3):
+        axes[ii].axvline(0.382,  color='grey', linestyle='--', alpha=0.7)
+    # plt.savefig(f'{fpath}/{fig_path}/objective_dist.png', dpi=300)
+    df_stats.to_csv(f'{fpath}{fig_path}/F_stats.csv')
 
+
+# import GP
+fpath = '../output/gp_run_0816/'
+gp = pickle.load(open(f'{fpath}gp_1.pkl', "rb"))
+x_training = gp.X_train_
+y_training = gp.y_train_
 
 # Resample in the ranges where the objective values are above 0
 x_select = x_training[np.where(y_training>0)[0], :]
@@ -156,11 +209,13 @@ if not os.path.exists(filename):
     interaction_terms.max(axis=0) == 1)[0]]
     sa = pyapprox.analytic_sobol_indices_from_gaussian_process(gp, variables, 
         interaction_terms, ngp_realizations=100, ninterpolation_samples=500, 
-            use_cholesky=True, ncandidate_samples=10000, nvalidation_samples=200)
-    # np.savez(filename, total_effects=sa['total_effects']['mean'])
+            use_cholesky=True, ncandidate_samples=10000, nvalidation_samples=200,
+            stat_functions=(np.mean, np.std))
+    np.savez(filename, total_effects=sa['total_effects']['values'])
     ST = sa['total_effects']['values']
     ST_mean = sa['total_effects']['mean']
     ST_mean = pd.DataFrame(data = ST_mean, index = param_names)
+    ST_mean['std'] = sa['total_effects']['std']
     ST_mean.to_csv(f'{fpath}ST.csv')
     index_sort = partial_rank(ST, ST.shape[1], conf_level=0.95)
     with open(filename, 'w') as fp:
@@ -173,4 +228,10 @@ else:
     for k, v in index_sort_load.items():
         index_sort[int(k)] = index_sort_load[k]
 
-fix_plot(gp, variable_temp)
+# Calculate the ratio of calibrating samples in the sub-region
+if not os.path.exists(f'{fpath}ratio_cali_subreg.csv'):
+    df = ratio_subreg(gp)
+    df.to_csv(f'{fpath}ratio_cali_subreg.csv')
+
+# Scatter plot and Pdf plot VS fixing parameters
+fix_plot(gp, variable_temp, plot_range='subreg')
